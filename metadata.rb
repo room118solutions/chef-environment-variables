b72ddc7f3f1f796372c355eb85ec996897c09496
Chris Gunther <chris@room118solutions.com>
2014-05-18 19:44:34 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'environment_variables'
+maintainer       'Room 118 Solutions'
+maintainer_email 'chris@room118solutions.com'
+license          'All rights reserved'
+description      'Defines environment variables in /etc/environment'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
